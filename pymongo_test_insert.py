c8b5264588279dc3927567d08a910e1a7716d648
anilreddy12001 <anilreddy12001@gmail.com>
2024-08-05 15:23:29 +0530

    Update pymongo_test_insert.py

--- pymongo_test_insert.py
+++ pymongo_test_insert.py
@@ -1,23 +1,31 @@
 # Get the database using the method we defined in pymongo_test_insert file
 from pymongo_get_database import get_database
 dbname = get_database()
-collection_name = dbname["user_1_items"]
+collection_name = dbname["sample_airbnb"]
 
 item_1 = {
-  "_id" : "U1IT00001",
-  "item_name" : "Blender",
-  "max_discount" : "10%",
-  "batch_number" : "RR450020FRG",
-  "price" : 340,
-  "category" : "kitchen appliance"
+  "_id" : "U1IT0009999",
+  "listing_url":
+"https://www.airbnb.com/rooms/1003530",
+"name":
+"New York City - Upper West Side Apt",
+"summary":
+"from python app",
+"space":
+"Murphy bed, optional second bedroom available. Wifi available, Hulu, N…",
+"description":
+"Murphy bed, optional second bedroom available. Wifi available, Hulu, N…",
+"neighborhood_overview":
+"Great neighborhood - many terrific restaurants, bakeries, bagelries. W…",
+"notes":
+"My cat, Samantha, are in and out during the summer.  The apt is layed …",
+"transit":
+"Conveniently located near 1, 2, 3, B & C subway lines. Also buses on C…",
+"access":
+"NYC!",
+"interaction":
+""
 }
 
-item_2 = {
-  "_id" : "U1IT00002",
-  "item_name" : "Egg",
-  "category" : "food",
-  "quantity" : 12,
-  "price" : 36,
-  "item_description" : "brown country eggs"
-}
+
 collection_name.insert_many([item_1,item_2])
